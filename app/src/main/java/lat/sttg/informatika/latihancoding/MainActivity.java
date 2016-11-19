1444d6dfb6e0dbcf85ebcc2175a3d62434ecda94
Aldy Rialdy <me@ald33.web.id>
2016-11-19 16:10:17 +0700

    Penambahan komentar pada program

--- app/src/main/java/lat/sttg/informatika/latihancoding/MainActivity.java
+++ app/src/main/java/lat/sttg/informatika/latihancoding/MainActivity.java
@@ -1,10 +1,14 @@
 package lat.sttg.informatika.latihancoding;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Ini program untuk tampilan result pemesanan
     private TextView textval;
 
     @Override
@@ -22,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
             order = b.getString("order");
 
         textval.setText(order+"\nPesanan anda totalnya = "+ value);
+    }
+    private Boolean exit = true;
+
+    @Override
+    public void onBackPressed() {
+        if (exit) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            startActivity(intent);
+        }
+
     }
 }
